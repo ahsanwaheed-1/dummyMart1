ba89608bb2940b6051c4f1077497b7f36faf3e0e
ahsanwaheed-1 <ahsanwaheed100@gmail.com>
2025-08-20 01:47:45 +0500

    Fixed vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-  import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -7,5 +7,8 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/',
+  server: {
+    host: true, 
+  },
+  base: '/', 
 });
